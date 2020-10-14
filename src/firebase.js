4382f5b6acc1adab545a78957584573115eb9917
awantech-io <hello.wanlab@gmail.com>
2020-10-14 22:17:07 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAH8DyF28exlbxoYRmvswUIJKAAa5F9YSE",
+    authDomain: "whatsapp-clone-879ec.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-879ec.firebaseio.com",
+    projectId: "whatsapp-clone-879ec",
+    storageBucket: "whatsapp-clone-879ec.appspot.com",
+    messagingSenderId: "921333342583",
+    appId: "1:921333342583:web:f75b133a7b916a3de48172",
+    measurementId: "G-PE0K7GGMZZ"
+  };
