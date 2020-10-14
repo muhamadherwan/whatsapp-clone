88728c501eef68be806c190c5833211d6b733f56
awantech-io <hello.wanlab@gmail.com>
2020-10-14 23:15:25 +0800

    sidebar header, sidebar header right

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      wan zuck
+    <div className="app">
+  
+      <div className='app__body'>
+      <Sidebar />
+      </div>
+
     </div>
   );
 }
